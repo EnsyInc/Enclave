0d393411a57648894ef40c60be9d549b348118c2
Stefan Tataran <fane_ecl@yahoo.com>
2026-08-08 15:42:34 +0300

    Refactor

--- src/EnsyInc.Enclave/EnsyInc.Enclave.DataAccess/Models/DbApp.cs
+++ src/EnsyInc.Enclave/EnsyInc.Enclave.DataAccess/Models/DbApp.cs
@@ -1,6 +1,4 @@
-﻿using EnsyInc.Enclave.Core.Models;
-
-using EnsyNet.DataAccess.Abstractions.Models;
+﻿using EnsyNet.DataAccess.Abstractions.Models;
 
 namespace EnsyInc.Enclave.DataAccess.Models;
 
@@ -8,25 +6,4 @@ public sealed record DbApp : DbEntity
 {
     public required string Name { get; init; }
     public string? Description { get; init; }
-}
-
-public static class AppExtensions
-{
-    public static DbApp ToDbApp(this App app) =>
-        new()
-        {
-            Id = app.Id,
-            Name = app.Name,
-            Description = app.Description,
-        };
-
-    public static App ToCoreApp(this DbApp dbApp) =>
-        new()
-        {
-            Id = dbApp.Id,
-            Name = dbApp.Name,
-            Description = dbApp.Description,
-            CreatedAt = dbApp.CreatedAt,
-            LastUpdatedAt = dbApp.UpdatedAt
-        };
 }
