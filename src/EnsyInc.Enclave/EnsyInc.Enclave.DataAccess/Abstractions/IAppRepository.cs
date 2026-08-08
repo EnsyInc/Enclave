9f07bb7954d483c9b137e5ad99443e422ddca967
Stefan Tataran <fane_ecl@yahoo.com>
2026-08-08 17:42:00 +0300

    Remove migration and app entity

--- src/EnsyInc.Enclave/EnsyInc.Enclave.DataAccess/Abstractions/IAppRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using EnsyInc.Enclave.DataAccess.Models;
-
-using EnsyNet.DataAccess.Abstractions.Interfaces;
-
-namespace EnsyInc.Enclave.DataAccess.Abstractions;
-
-public interface IAppRepository : IRepository<DbApp>;
